de0e6bba5977616c3335df3ab8b3254231401862
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-05-02 12:46:31 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+    
+        Stack<string> parole = new Stack<string>(new string[] { "Ciao", "Mondo", "Come", "Stai" });
+        string parolaRimossa = parole.Pop();
+
+        Console.WriteLine("Stack aggiornato:");
+        foreach (string parola in parole)
+        {
+            Console.WriteLine(parola);
+        }
